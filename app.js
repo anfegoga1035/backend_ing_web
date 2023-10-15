f4bce5aab1c919e730f04c121f672de1c610e988
anfegoga1035 <anfegoga1035@gmail.com>
2023-10-15 15:51:16 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const { mongoConn } = require('./databases/configuration')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+
+mongoConn()
+
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(cors({
+    origin: '*'
+}))
+
+const test = require('./routes/test')
+const generos = require('./routes/genero')
+
+app.use('/api/v1/tests', test)
+app.use('/api/v1/generos', generos)
+
+module.exports = app
---
+++ routes/genero.js
@@ -0,0 +1,28 @@
+const { Router } = require('express')
+const {createGenero, getGeneros} = require('../controllers/genero')
+
+const router = Router()
+
+/**
+ * Crear genero
+ */
+router.post('/', createGenero)
+
+/**
+ * consultar todos los géneros
+ */
+router.get('/', getGeneros)
+
+/**
+ * Consultar un género por su ID
+ */
+
+/**
+ * Actualizar genero
+ */
+
+/**
+ * Borrar un género
+ */
+
+module.exports = router
